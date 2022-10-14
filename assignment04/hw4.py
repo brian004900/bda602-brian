ac6ee105f974ecc266163241012bc497016dfd9d
Brian <studybc870409@gmail.com>
2022-10-14 01:33:36 -0700

    brian_i did my best_final table has created

--- assignment04/hw4.py
+++ assignment04/hw4.py
@@ -3,10 +3,10 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 import statsmodels.api
 from plotly import express as px
 from plotly import figure_factory as ff
-from plotly import graph_objects as go
 from sklearn import datasets
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import confusion_matrix
@@ -15,11 +15,16 @@ from sklearn.preprocessing import LabelEncoder
 
 def main():
 
-    data = datasets.load_breast_cancer()
+    data = datasets.load_iris()
     df = pd.DataFrame(data["data"], columns=data["feature_names"])
     B = data["target"]
-
     B_df = pd.DataFrame(B)
+
+    finaltable = pd.DataFrame(
+        index=data["feature_names"],
+        columns=["feature_names", "mse", "wmse", "t_value", "p_value", "random_forest"],
+    )
+    finaltable["feature_names"] = data["feature_names"]
 
     for att in df.columns:
         if df[att].dtype.kind in "OM":
@@ -39,6 +44,95 @@ def main():
         response = 1  # cat
     else:
         response = 0
+
+    # calculate mse and wmse
+    for att in df.columns:
+
+        mselist = []
+        ilist = []
+        numlist = []
+        wmselist = []
+
+        df_cut = pd.DataFrame()
+        df_cut["predictor"] = df[att]
+        df_cut["response"] = data["target"]
+        label_names = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        df_max = df_cut["predictor"].max()
+        df_min = df_cut["predictor"].min()
+        pop_mean = df_cut["response"].mean()
+        df_cut["popmean"] = pop_mean
+        cut_points = []
+        cut_center = []
+        popmeanlist = []
+
+        cut_points.append(df_min)
+        cut_dis = (df_max - df_min) / 10
+
+        for i in range(10):
+            df_min = df_min + cut_dis
+            cut_points.append(df_min)
+        df_cut["binning"] = pd.cut(
+            x=df_cut["predictor"], bins=cut_points, labels=label_names
+        )
+
+        for i in range(10):
+            center = (cut_points[i] + cut_points[i + 1]) / 2
+            cut_center.append(center)
+
+        for i in range(10):
+            popmeanlist.append(pop_mean)
+
+        for i in range(10):
+            bin1 = df_cut[df_cut.binning == str(i)]
+
+            array1 = np.array(bin1["response"])
+            array2 = np.array(bin1["popmean"])
+
+            if len(bin1) == 0:
+                mse = 0
+                wmse = 0
+
+            else:
+                difference_array = np.subtract(array1, array2)
+                squared_array = np.square(difference_array)
+                mse = squared_array.mean()
+            w = len(bin1) / len(df_cut)
+            wmse = mse * w
+            ilist.append(i)
+            mselist.append(mse)
+            wmselist.append(wmse)
+            numlist.append(len(bin1))
+
+        finaltable.loc[att, "mse"] = sum(mselist) / 10
+        finaltable.loc[att, "wmse"] = sum(wmselist) / 10
+
+        fig = go.Figure(
+            layout=go.Layout(
+                title="Binned difference with mean of response vs mean",
+                yaxis2=dict(overlaying="y"),
+            )
+        )
+        fig.add_trace(go.Bar(x=cut_center, y=numlist, yaxis="y1"))
+        fig.add_trace(
+            go.Scatter(
+                x=cut_center,
+                y=mselist,
+                yaxis="y2",
+                mode="lines",
+                line=go.scatter.Line(color="red"),
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=cut_center,
+                y=popmeanlist,
+                yaxis="y2",
+                mode="lines",
+                line=go.scatter.Line(color="green"),
+                showlegend=False,
+            )
+        )
+        fig.show()
 
     for att in df.columns:
         likely_cat = 1.0 * df[att].nunique() / df[att].count() < 0.05
@@ -62,7 +156,6 @@ def main():
     if not con_p.empty and response == 1:
         plot_df = pd.DataFrame(con_p)
         plot_df["target"] = data["target"]
-
         for i in plot_df.iloc[:, :-1]:
             hist_data = []
             classes = plot_df.groupby("target")[i].apply(list)
@@ -78,7 +171,7 @@ def main():
                 xaxis_title="Predictor",
                 yaxis_title="Distribution",
             )
-            fig_1.show()
+            # fig_1.show()
 
             n = len(plot_df)
             fig_2 = go.Figure()
@@ -108,7 +201,7 @@ def main():
                 xaxis_title="Predictor",
                 yaxis_title="Response",
             )
-            fig.show()
+            # fig.show()
 
     # plot cat/con
     if not cat_p.empty and response == 0:
@@ -127,7 +220,7 @@ def main():
                 xaxis_title="Response",
                 yaxis_title="Distribution",
             )
-            fig_1.show()
+            # fig_1.show()
 
             n = len(plot_df)
             fig_2 = go.Figure()
@@ -146,7 +239,7 @@ def main():
                 xaxis_title="Groupings",
                 yaxis_title="Response",
             )
-            fig_2.show()
+            # fig_2.show()
 
     # plot cat/cat
     if not cat_p.empty and response == 1:
@@ -160,15 +253,19 @@ def main():
             xaxis_title="Response",
             yaxis_title="Predictor",
         )
-        fig_no_relationship.show()
+        # fig_no_relationship.show()
 
     # Regression: boolean #dataset:load_breast_cancer()
-
+    del con_p["target"]
+    con_X = np.array(con_p)
+    y = np.divide(y - y.min(), y.max() - y.min())
     if not con_p.empty and response == 1:
-        for idx, column in enumerate(X_con.T):
+        for idx, column in enumerate(con_X.T):
             feature_name = list(con_p.columns)[idx]
             predictor = statsmodels.api.add_constant(column)
-            linear_regression_model = statsmodels.api.Logit(y, predictor)
+            linear_regression_model = statsmodels.api.Logit(
+                y, predictor, missing="drop"
+            )
             linear_regression_model_fitted = linear_regression_model.fit()
             print(f"Variable: {feature_name}")
             print(linear_regression_model_fitted.summary())
@@ -176,6 +273,8 @@ def main():
             # Get the stats
             t_value = round(linear_regression_model_fitted.tvalues[1], 6)
             p_value = "{:.6e}".format(linear_regression_model_fitted.pvalues[1])
+            finaltable.loc[feature_name, "t_value"] = t_value
+            finaltable.loc[feature_name, "p_value"] = p_value
 
             # Plot the figure
             fig = px.scatter(x=column, y=y, trendline="lowess")
@@ -200,6 +299,9 @@ def main():
             t_value = round(linear_regression_model_fitted.tvalues[1], 6)
             p_value = "{:.6e}".format(linear_regression_model_fitted.pvalues[1])
 
+            finaltable.loc[feature_name, "t_value"] = t_value
+            finaltable.loc[feature_name, "p_value"] = p_value
+
             # Plot the figure
             fig = px.scatter(x=column, y=y, trendline="ols")
             fig.update_layout(
@@ -212,10 +314,16 @@ def main():
     # random forest ranking
     if not con_p.empty:
         rf = RandomForestRegressor(n_estimators=50)
-        rf.fit(X_con, y)
-        print(rf.feature_importances_)
+        rf.fit(con_X, y)
+        for i in range(len(list(con_p.columns))):
+            finaltable.loc[
+                list(con_p.columns)[i], "random_forest"
+            ] = rf.feature_importances_[i]
         plt.barh(list(con_p.columns), rf.feature_importances_)
         plt.show()
+
+    # print final table!!!!!
+    print(finaltable)
 
 
 if __name__ == "__main__":
